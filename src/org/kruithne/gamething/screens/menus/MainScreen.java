394e883a136064f026a81a57d4fc898684b8f631
Kruithne <kruithne@gmail.com>
2013-12-14 04:18:01 +0000

    Just go for a solid background colour.

--- src/org/kruithne/gamething/screens/menus/MainScreen.java
+++ src/org/kruithne/gamething/screens/menus/MainScreen.java
@@ -1,8 +1,7 @@
 package org.kruithne.gamething.screens.menus;
 
-import org.kruithne.gamething.helpers.ImageLoader;
-import org.kruithne.gamething.helpers.RenderHelper;
 import org.kruithne.gamething.screens.IMenuScreen;
+import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 
@@ -11,6 +10,6 @@ public class MainScreen implements IMenuScreen
 	@Override
 	public void draw(GameContainer window, Graphics renderer)
 	{
-		RenderHelper.renderBackground(window, renderer, ImageLoader.getImage("dirt_background.png"));
+		renderer.setBackground(new Color(138, 89, 49));
 	}
 }
