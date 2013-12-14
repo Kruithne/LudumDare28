3b8b5fd8a3da362a8d984260ab1e8954aa6e0ddd
Kruithne <kruithne@gmail.com>
2013-12-14 04:32:33 +0000

    Added in a logo.

--- src/org/kruithne/gamething/screens/menus/MainScreen.java
+++ src/org/kruithne/gamething/screens/menus/MainScreen.java
@@ -1,9 +1,11 @@
 package org.kruithne.gamething.screens.menus;
 
+import org.kruithne.gamething.helpers.ImageLoader;
 import org.kruithne.gamething.screens.IMenuScreen;
 import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
 
 public class MainScreen implements IMenuScreen
 {
@@ -11,5 +13,8 @@ public class MainScreen implements IMenuScreen
 	public void draw(GameContainer window, Graphics renderer)
 	{
 		renderer.setBackground(new Color(81, 65, 53));
+
+		Image logo = ImageLoader.getImage("logo.png");
+		renderer.drawImage(logo, (window.getWidth() / 2) - (logo.getWidth() / 2), 25);
 	}
 }
